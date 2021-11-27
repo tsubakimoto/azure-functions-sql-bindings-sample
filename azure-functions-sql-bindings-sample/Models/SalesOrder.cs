5c555ab4415626e15abb25723ccb261aa96c8629
tsubakimoto <tkmbmbr@gmail.com>
2021-11-27 11:02:59 +0900

    Add join query

---
+++ azure-functions-sql-bindings-sample/Models/SalesOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace azure_functions_sql_bindings_sample.Models
+{
+    public class SalesOrder
+    {
+        public int SalesOrderID { get; set; }
+        public int SalesOrderDetailId { get; set; }
+        public int OrderQty { get; set; }
+    }
+}
