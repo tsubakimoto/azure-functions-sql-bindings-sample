ff21d98992915c1839e0a4e62fac8ea6db5a6880
tsubakimoto <tkmbmbr@gmail.com>
2021-11-27 10:29:50 +0900

    Add view binding

---
+++ azure-functions-sql-bindings-sample/Models/CategoryView.cs
@@ -0,0 +1,9 @@
+﻿namespace azure_functions_sql_bindings_sample.Models
+{
+    public class CategoryView
+    {
+        public string ParentProductCategoryName { get; set; }
+        public string ProductCategoryName { get; set; }
+        public int ProductCategoryID { get; set; }
+    }
+}
