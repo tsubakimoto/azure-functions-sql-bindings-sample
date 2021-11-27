813be2f2dccbc1859c306c7731fc5c65c3f68c33
tsubakimoto <tkmbmbr@gmail.com>
2021-11-27 10:38:42 +0900

    Fix content type

--- azure-functions-sql-bindings-sample/GetCategoryView.cs
+++ azure-functions-sql-bindings-sample/GetCategoryView.cs
@@ -25,7 +25,7 @@ namespace azure_functions_sql_bindings_sample
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The OK response")]
         public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             [Sql("select * from SalesLT.vGetAllCategories where ParentProductCategoryName = @Name",
