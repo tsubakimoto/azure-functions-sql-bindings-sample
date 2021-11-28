362e825187a842913665a099f482e8b88d70d5f2
tsubakimoto <tkmbmbr@gmail.com>
2021-11-28 22:48:56 +0900

    Add CreateProductModel (WIP)

---
+++ azure-functions-sql-bindings-sample/Models/ProductModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace azure_functions_sql_bindings_sample.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
