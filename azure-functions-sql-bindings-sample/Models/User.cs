5422296d2964a22939302ad05c73a7b3f09d5e97
Yuta Matsumura <tkmbmbr@gmail.com>
2021-11-30 22:26:53 +0900

    Add simple table output bindings

---
+++ azure-functions-sql-bindings-sample/Models/User.cs
@@ -0,0 +1,15 @@
+﻿/*
+CREATE TABLE User (
+    Name nvarchar not null primary key,
+    Age int not null
+)
+*/
+
+namespace azure_functions_sql_bindings_sample.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
